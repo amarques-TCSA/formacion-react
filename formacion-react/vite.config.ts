a85a8c8e5e59f7533fc821014959e139aa52b166
Adrián Jiménez Galán <adjimenez@itracasa.es>
2026-05-06 12:13:31 +0200

    chore: añadir alias

--- formacion-react/vite.config.ts
+++ formacion-react/vite.config.ts
@@ -1,14 +1,19 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import { mockDevServerPlugin } from 'vite-plugin-mock-dev-server'
-
+import * as path from 'path';
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [
     react(),
-    mockDevServerPlugin(), 
+    mockDevServerPlugin(),
   ],
+  resolve: {
+      alias: [
+          { find: '@', replacement: path.resolve(__dirname, 'src') },
+      ]
+  },
   server: {
     proxy: {
       '^/api': 'http://example.com/'
