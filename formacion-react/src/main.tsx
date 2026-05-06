794dd3c8b4cc0ca905bf64156f2af427620dffa5
Adrián Jiménez Galán <adjimenez@itracasa.es>
2026-05-06 07:40:26 +0200

    feat: incluir estilos

--- formacion-react/src/main.tsx
+++ formacion-react/src/main.tsx
@@ -4,6 +4,7 @@ import { ThemeProvider } from '@tracasa/tracasa-components'
 import CustomErrorBoundary from './shared/base/error-boundary.tsx'
 import App from './App.tsx'
 import './index.css'
+import '@tracasa/tracasa-components/styles';
 import '@tracasa/tracasa-components/styles/navarra';
 import './shared/config/i18n';
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
@@ -13,7 +14,7 @@ export const queryClient = new QueryClient();
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
      <CustomErrorBoundary>
-      <ThemeProvider defaultTheme="avantius">
+      <ThemeProvider defaultTheme="navarra">
         <QueryClientProvider client={queryClient}>
           <App />
         </QueryClientProvider>
